c03cffa4d43ab101123fadc9fed166a554266f97
Isaac <mayne.isaac@gmail.com>
2025-03-09 22:55:47 +0100

    firs commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/sonner"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="w-full bg-[#F3F3F1] h-full min-h-screen">{children}</main>
         <Toaster />
       </body>
     </html>
